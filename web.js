e6514c618f2c59623e5d70501f126e630c68962d
B Cacheira <bcacheira@gmail.com>
2013-07-09 21:06:57 +0100

    Part 1 done

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+function read_index(){
+    var buffer = new Buffer(255);
+    buffer = fs.readFileSync('index.html');
+    //console.log("File contents:" + buffer);
+    return buffer.toString('utf-8');
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    
+    response.send(read_index());
 });
 
 var port = process.env.PORT || 5000;
